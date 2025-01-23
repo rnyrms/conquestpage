2fd6c17d6f8b99d9da29e58e5008d2d3b6ae2718
Rony <76442796+rnyrms@users.noreply.github.com>
2025-01-23 15:48:36 -0300

    Add files via upload

---
+++ wp-content/uploads/uag-plugin/assets/0/uag-js-137.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", function(){ window.addEventListener( 'load', function() {
+	UAGBButtonChild.init( '.uagb-block-515732c6' );
+});
+window.addEventListener( 'load', function() {
+	UAGBButtonChild.init( '.uagb-block-5cwicgc6' );
+});
+window.addEventListener( 'load', function() {
+	UAGBButtonChild.init( '.uagb-block-wznzwcw7' );
+});
+window.addEventListener( 'load', function() {
+	UAGBButtonChild.init( '.uagb-block-rpqahwls' );
+});
+ });
